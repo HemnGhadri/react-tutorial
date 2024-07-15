4a1d36218e44be6a237fc2f5b17f3ee0a713f631
Hemn <hemn.ghadri@gmail.com>
2024-07-15 14:41:46 +0330

    start react with class and function component

---
+++ tutorial/src/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Component} from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>App Component</h1>
+                <h2>App Component</h2>
+            </React.Fragment>
+        );
+    }
+}
+
+export default App;
---
+++ tutorial/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import App from './app';
+import AppFunc from './app-func';
+// const element = <h1>React element</h1>
+
+
+ReactDOM.render(<AppFunc></AppFunc>, document.getElementById("root"));
