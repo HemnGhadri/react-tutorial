55fbc17a03307ceb17d9ffd45962fd25e3f79d3c
Hemn <hemn.ghadri@gmail.com>
2024-08-04 21:25:07 +0330

    use context to pass data between component

--- tutorial/src/components/products.jsx
+++ tutorial/src/components/products.jsx
@@ -1,61 +1,22 @@
 import React from "react";
 import Product from "./product";
+import ProductContext from "./context/prodocts";
 
 class Products extends React.Component {
 
-    state = {
-        products:
-            [
-                {id: 1, productName: "laptop", count: 7},
-                {id: 2, productName: "phone", count: 1},
-                {id: 3, productName: "airpods", count: 5},
-            ]
-    }
+    static contextType = ProductContext
 
     render() {
         return (
             <div>
-                {this.state.products.map((p, index) => (
-                    <Product onDelete={this.handleDelete} id={p.id} key={index} productName={p.productName}
-                             count={p.count} onReset={this.handleReset} onDecrement={this.handleDecrement}
-                             onIncrement={this.handelIncrement}> {p.productName} description
+                {this.context.products.map((p, index) => (
+                    <Product onDelete={this.context.onDelete} id={p.id} key={index} productName={p.productName}
+                             count={p.count} onReset={this.context.onReset} onDecrement={this.context.onDecrement}
+                             onIncrement={this.context.onIncrement}> {p.productName} description
                         ...</Product>
                 ))}
             </div>
         )
-    }
-
-    handleDelete = (productId) => {
-        const newProducts = this.state.products.filter(p => p.id !== productId);
-        this.setState({products: newProducts});
-    }
-
-    handleReset = (productId) => {
-        const newProducts = this.state.products.map(p => {
-            p.count = p.id === productId ? 0 : p.count;
-            return p
-        });
-        console.log(newProducts);
-        this.setState({products: newProducts});
-        console.log("reset")
-    }
-    handleDecrement = (productId) => {
-        const newProducts = this.state.products.map(p => {
-            if (p.id === productId) {
-                p.count = p.count - 1
-            }
-            return p
-        });
-        this.setState({products: newProducts})
-    }
-    handelIncrement = (productId) => {
-        const newProducts = this.state.products.map(p => {
-            if (p.id === productId) {
-                p.count = p.count + 1
-            }
-            return p
-        });
-        this.setState({products: newProducts})
     }
 }
 
--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
-// import App from './app';
-// import AppFunc from './app-func';
-// import Products from './components/product'
-// import Products from './components/functional/product'
-// import Products from './components/products'
-import Products from './components/functional/productsFunctional'
+import App from "./app";
 import 'bootstrap/dist/css/bootstrap.css';
 // const element = <h1>React element</h1>
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(<Products></Products>)
+root.render(<App></App>)
