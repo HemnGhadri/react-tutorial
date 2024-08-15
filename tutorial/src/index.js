d95d48d353c04189336e23312d582861fda6cf12
Hemn <hemn.ghadri@gmail.com>
2024-08-15 11:03:07 +0330

    change location files for add online users projects

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
-import App from "./app";
+import App from "./learning/app";
 import 'bootstrap/dist/css/bootstrap.css';
 // const element = <h1>React element</h1>
 
